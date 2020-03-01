2cfbc5ec6e57a0223d0f7a3123b1b44d16a772c6
Stuart <stuart.wainstock@gmail.com>
2020-03-01 15:16:30 -0600

    if, else, show

--- js/main.js
+++ js/main.js
@@ -4,6 +4,9 @@ let app = new Vue({
 		product: 'Socks',
 		description: 'A pair of warm, fuzzy socks',
 		image: './assets/vmSocks-green-onWhite.jpg',
-		altText: 'Green socks'
+		altText: 'Green socks',
+		inStock: true,
+		inventory: 12,
+		onSale: true
 	}
 })
