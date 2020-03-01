486af9085abc1090a9443f54027a2d1d8fce7c39
Stuart <stuart.wainstock@gmail.com>
2020-03-01 16:03:00 -0600

    added blue socks and p event listener to toggle product image

--- js/main.js
+++ js/main.js
@@ -12,11 +12,13 @@ let app = new Vue({
 		variants: [
 			{
 				variantId: 2234,
-				variantColor: "green"
+				variantColor: "green",
+				variantImage: "./assets/vmSocks-green-onWhite.jpg"
 			},
 			{
 				variantId: 2235,
-				variantColor: "blue"
+				variantColor: "blue",
+				variantImage: "./assets/vmSocks-blue-onWhite.jpg"
 			}
 			],
 			cart: 0
@@ -24,6 +26,9 @@ let app = new Vue({
 		methods: {
 			addToCart: function (){
 				this.cart += 1
+			},
+			updateProduct: function (variantImage) {
+				this.image = variantImage
 			}
 		}
 })
